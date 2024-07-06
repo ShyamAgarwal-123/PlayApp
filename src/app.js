f14184267871471b0db85931a8f22aac0926b25c
Shyam Agarwal <shyamagarwal3800@gmail.com>
2024-07-06 18:51:01 +0530

    setup completed

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import cookieParser from "cookie-parser";
 
+
 const app = express();
 
 
@@ -15,5 +16,14 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({limit: "16kb",extended: true}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+//http://localhost:8000/api/v1/users then calls the userRouter
 
 export {app};
